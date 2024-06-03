9b43bea1955490ddadf677de4a2d711e62c91176
q8wahab <q8_xprt@hotmail.com>
2024-06-03 18:18:54 +0300

    fixing the date and time format

--- src/pages/Transactions.js
+++ src/pages/Transactions.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getMyTransactions, me } from "../api/auth";
+import days from "dayjs";
+import LocalizedFormat from "dayjs/plugin/localizedFormat";
+
+days.extend(LocalizedFormat);
 
 const Transactions = () => {
   const { data: userData } = useQuery({
@@ -8,10 +12,14 @@ const Transactions = () => {
     queryFn: () => me(),
   });
 
-  const { data: transactionsData } = useQuery({
+  const { data: transactionsData, isLoading } = useQuery({
     queryKey: ["transactions"],
     queryFn: getMyTransactions,
   });
+
+  if (isLoading) {
+    return <h1>loading...</h1>;
+  }
 
   return (
     <div>
@@ -31,7 +39,7 @@ const Transactions = () => {
               <tr key={index}>
                 <th>{index + 1}</th>
                 <td>{transaction.amount}</td>
-                <td>{transaction.createdAt}</td>
+                <td>{days(transaction.createdAt).format("LLL")}</td>
                 <td>{transaction.type}</td>
               </tr>
             ))}
